30570d153ab970f71e6fe1b01686a690e8efbfae
Satya Surya <ssurya@onemarketnetwork.com>
2018-04-26 21:51:54 -0700

    Added about and donate VC

--- tattvamasi/ViewControllers/AboutUsViewController.swift
+++ tattvamasi/ViewControllers/AboutUsViewController.swift
@@ -1,25 +1,25 @@
 //
-//  ViewController.swift
+//  AboutUsViewController.swift
 //  tattvamasi
 //
-//  Created by Satya Surya on 4/24/18.
+//  Created by Satya Surya on 4/26/18.
 //  Copyright © 2018 Satya Surya. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class AboutUsViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
---
+++ tattvamasi/ViewControllers/DonateViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DonateViewController.swift
+//  tattvamasi
+//
+//  Created by Satya Surya on 4/26/18.
+//  Copyright © 2018 Satya Surya. All rights reserved.
+//
+
+import UIKit
+
+class DonateViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
